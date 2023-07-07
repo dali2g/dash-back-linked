0f0b8de71c0584cb4ded265c14247d15eae2da92
dali2g <mamejdi12@gmail.com>
2023-07-07 19:19:31 +0100

    (1.0.1) - Adding Initial Files

---
+++ src/controllers/userController.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function userController() {
+  return (
+    <div>userController</div>
+  )
+}
+
+export default userController
---
+++ src/models/userModel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function userModel() {
+  return (
+    <div>userModel</div>
+  )
+}
+
+export default userModel
